ec4ebbb0fc9154c55606217e58fc21c89ac8a3b0
Thorin Butler <thorin.butler@gmail.com>
2016-06-01 21:27:55 -0500

    There was an error that was kicked up, looked into code and corrected token

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,9 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
- 
+
 import Task from './Task.jsx';
- 
+
 // App component - represents the whole app
 class App extends Component {
   renderTasks() {
@@ -12,14 +12,14 @@ class App extends Component {
       <Task key={task._id} task={task} />
     ));
   }
-}
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
         </header>
- 
+
         <ul>
           {this.renderTasks()}
         </ul>
@@ -27,11 +27,11 @@ class App extends Component {
     );
   }
 }
- 
+
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
 };
- 
+
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}).fetch(),
