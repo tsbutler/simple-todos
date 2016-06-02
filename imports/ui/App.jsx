c7aa51c33179aa282ff4eba2151a371a7f352489
Thorin Butler <thorin.butler@gmail.com>
2016-06-02 16:14:40 -0500

    Updated data container to show newest tasks at top

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -22,7 +22,7 @@ class App extends Component {
     // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = ''
    }
-   
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -58,6 +58,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
