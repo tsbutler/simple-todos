b1c09ecaafa4d7924ca01b2f443cc405e56e9683
Thorin Butler <thorin.butler@gmail.com>
2016-06-09 14:05:43 -0500

    added private className to Task when needed

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
  
 // Task component - represents a single todo item
 export default class Task extends Component {
@@ -15,11 +16,14 @@ export default class Task extends Component {
   togglePrivate() {
     Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
   }
-  
+
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
 
     return (
       <li className={taskClassName}>
